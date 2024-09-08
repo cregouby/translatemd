d16a9af53e699528441dd0acf0b830e75480c48b
C. Regouby <christophe.regouby@free.fr>
2024-09-08 19:37:55 +0200

    global machinery is OK. Result is poor

--- R/select_lang.R
+++ R/select_lang.R
@@ -2,7 +2,7 @@
 #' 
 #' for example "Helsinki-NLP/opus-mt-en-es"
 #' @export
-#' @param lang
+#' @param lang translation model to use like "Helsinki-NLP/opus-mt-en-es"
 #' @importFrom reticulate import
 select_lang <- function(lang){
   translator <- transformers$pipeline(task = "translation", model=lang)
